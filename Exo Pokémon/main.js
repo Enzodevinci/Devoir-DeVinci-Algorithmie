b9f78c4a141e47c4609b69652460f0ac5fd01b9e
Enzodevinci <149313367+Enzodevinci@users.noreply.github.com>
2023-10-29 15:18:44 +0100

    Add files via upload

---
+++ Exo Pokémon/main.js
@@ -0,0 +1,44 @@
+alert("Je fonctionne Bien")
+
+class pokémon {
+    constructor (name, attack, defense, hp, luck){
+        this.name = name
+        this.attack = attack
+        this.defense = defense
+        this.hp = hp
+        this.luck = luck
+    }
+    attackPokemon(pokémon) {
+        if (this.isLucky()) {
+            let damage = this.attack - pokémon.defense
+            pokémon.hp -= damage
+            console.log(this.name + ' a attaqué ' + pokémon.name + ' et reçu ' + damage + ' points de dégâts ! Il lui reste ' + pokémon.hp + ' % points de vie')
+        } else {
+            console.log(this.name + ' à raté son attaque !')
+        }
+    }
+
+    isLucky(){
+        return this.luck > Math.random()
+    }
+}
+
+let umbreon = new pokémon('Umbreon', 12, 10, 100, 0.9)
+let sylveon = new pokémon('Sylveon', 14, 8, 100, 0.9)
+
+while (umbreon.hp > 0 && sylveon.hp > 0) {
+
+    sylveon.attackPokemon(umbreon)
+
+    if (umbreon.hp <= 0) {
+        console.log(umbreon.name + ' est KO !')
+        break
+    }
+
+    umbreon.attackPokemon(sylveon)
+
+    if (sylveon.hp <= 0) {
+        console.log(sylveon.name + ' est KO !')
+        break
+    }
+}
---
+++ Exo Taxi/main.js
@@ -0,0 +1,26 @@
+alert("Je fonctionne Bien")
+
+var john = 10
+var feuRouge = 0
+const musics = ['LOAT - JOYCA, Cjae', 'Counting Star - OneRepublic', 'LET GO - Central Cee', '7 Years - Lukas Graham', 'Anissa - Wejdene']
+var change = 0
+
+while ( feuRouge < 30 && john > 0){
+    for(let i = 0; i < 5; i++){
+        console.log(musics[i],feuRouge++)
+            console.log (' John écoute la Musique')
+            if(musics [4] === musics[i] ){
+                john -= 1
+                change += 1
+                console.log (' John a perdu 1  de santé Mental, et il change de Taxi et il lui reste ' + john + ' de santé Mental !')
+            }
+            if(john == 0){
+                console.log (' John explose ! ')
+                break
+                }
+            if(feuRouge == 30){
+                console.log('John est arrivé chez lui, il changé ' + change + ' fois de Taxi durant le Trajet ! ')
+                break
+            }
+        }
+    }
